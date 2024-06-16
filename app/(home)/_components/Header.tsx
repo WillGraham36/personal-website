a08b8e31f798872c31013034819316d51663f537
Will <willgraham367@gmail.com>
2024-06-16 17:58:03 -0400

    made the layout responsive to mobile

--- app/(home)/_components/Header.tsx
+++ app/(home)/_components/Header.tsx
@@ -3,17 +3,17 @@ import PFP from "../../../public/PFP.jpg";
 
 const Header = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
-        <Image 
+    <div className="w-full h-full flex justify-center items-center flex-col md:flex-row">
+        {/* <Image 
             src={PFP}
             alt="Will Graham"
-            className="rounded-full"
-        />
-        <div className="px-8">
-            <h1 className="text-primary-foreground text-[75px] font-bold">
+            className="rounded-full max-w-[20%] h-auto"
+        /> */}
+        <div className="px-8 pb-20 md:pb-6">
+            <h1 className="text-primary-foreground text-5xl md:text-8xl font-bold pb-4">
                 Hi, I'm Will
             </h1>
-            <h2 className=" text-muted font-medium text-[30px]">
+            <h2 className="text-muted font-medium md:text-[35px] text-lg">
                 I'm a Computer Science student at the University of Maryland <br/>and full-stack web developer
             </h2>
         </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,29 @@
 "use client";
 import { Button } from "./ui/button"
 import { useRouter } from "next/navigation"
+import { useMediaQuery } from 'usehooks-ts'
 
 const Navbar = () => {
     const router = useRouter();
+    const isMobile = useMediaQuery("(max-width: 768px)");
+    const size = isMobile ? "sm" : "lg";
 
   return (
-    <div className="w-full text-primary-foreground z-[99999] bg-offmain p-2 px-3 flex justify-between items-center top-0">
-        <h1 className="text-3xl font-medium">Will Graham</h1>
+    <div className="w-full text-primary-foreground z-[99999] bg-offmain p-2 px-3 flex justify-center md:justify-between items-center top-0">
+        {!isMobile &&
+            <h1 className="text-3xl font-medium">Will Graham</h1>
+        }
         <div>
-            <Button size={"lg"} variant={"ghost"} onClick={() => {router.push('./')}}>
+            <Button size={size} variant={"ghost"} onClick={() => {router.push('./')}}>
                 Home
             </Button>
-            <Button size={"lg"} variant={"ghost"} onClick={() => {router.push("./projects")}}>
+            <Button size={size} variant={"ghost"} onClick={() => {router.push("./projects")}}>
                 Projects
             </Button>
-            <Button size={"lg"} variant={"ghost"} onClick={() => {router.push("./about")}}>
+            <Button size={size} variant={"ghost"} onClick={() => {router.push("./about")}}>
                 About
             </Button>
-            <Button size={"lg"} variant={"ghost"} onClick={() => {router.push("./contact")}}>
+            <Button size={size} variant={"ghost"} onClick={() => {router.push("./contact")}}>
                 Contact
             </Button>
         </div>
