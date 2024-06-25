2a301ac04cfc34924d3027199ea7572942d20f78
Will <willgraham367@gmail.com>
2024-06-24 23:52:39 -0400

    refactor: added keys to iterated elements

--- app/projects/_components/Projects.tsx
+++ app/projects/_components/Projects.tsx
@@ -5,17 +5,17 @@ const Projects = async () => {
 
     const file = await fs.readFile(process.cwd() + '/app/projects/projects-list.json', 'utf8');
     const data: { [key: string]: any }[] = JSON.parse(file);
-	// project fields
-	//
+    // project fields
+    //
     // id: string;
-	// name: string;
+    // name: string;
     // title: string;
-	// description: string;
-	// image: string;
-	// technologies: string[];
-	// github: string;
-	// website: string;
-	//
+    // description: string;
+    // image: string;
+    // technologies: string[];
+    // github: string;
+    // website: string;
+    //
 
 
   return (
@@ -27,28 +27,10 @@ const Projects = async () => {
                     description={project[1].description} 
                     image={project[1].image} 
                     url={project[0]} 
+                    key={project[1].title}
                 />
             )
         })}
-
-        {/* <Card 
-          title={"Caprice - A Notion Clone"} 
-          description={testString} 
-          image={capriceCover} 
-          url={"caprice"} 
-        />
-        <Card 
-          title={"Food Finder"} 
-          description={testString} 
-          image={foodFinderCover} 
-          url={"food-finder"} 
-        />
-        <Card 
-          title={"Spotify Artist Playlist Creator"} 
-          description={testString} 
-          image={spotifyArtistCreatorCover} 
-          url={"spotify"} 
-        /> */}
   </div>
   )
 }
