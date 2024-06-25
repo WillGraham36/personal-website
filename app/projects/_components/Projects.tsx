d34965a7eb4688a17fcecc00b2b55bb0c2fde126
Will <willgraham367@gmail.com>
2024-06-25 01:18:32 -0400

    refactor: changed how data is pulled from project-list.json to work correctly on production

--- app/projects/_components/Projects.tsx
+++ app/projects/_components/Projects.tsx
@@ -5,7 +5,7 @@ const Projects = async () => {
 
     const file = await fs.readFile(process.cwd() + '/app/projects/projects-list.json', 'utf8');
     const data: { [key: string]: any }[] = JSON.parse(file);
-
+    
     // project fields
     //
     // id: string;
@@ -19,21 +19,23 @@ const Projects = async () => {
     //
 
 
-  return (
-    <div className="flex items-end justify-center flex-wrap pt-16 gap-10">
-        {Object.entries(data).map((project) => {
-            return (
-                <Card 
-                    title={project[1].title} 
-                    description={project[1].description} 
-                    image={project[1].image} 
-                    url={project[0]} 
-                    key={project[1].title}
-                />
-            )
-        })}
-  </div>
-  )
+    return (
+        <div className="flex items-end justify-center flex-wrap pt-16 gap-10">
+            {Object.entries(data).map((project) => {
+                return (
+                    <Card 
+                        title={project[1].title} 
+                        description={project[1].description} 
+                        image={project[1].image} 
+                        url={project[0]} 
+                        key={project[1].title}
+                    />
+                )
+            })}
+        </div>
+    )
+
+
 }
 
 export default Projects
