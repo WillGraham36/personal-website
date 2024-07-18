1e568436a70f0ad294075a9a632ab95df75a29bf
Will <willgraham367@gmail.com>
2024-07-18 01:47:40 -0400

    build: removed unecessary about me facts and adjusted constent slightly

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -2,11 +2,8 @@
 import { useMediaQuery } from 'usehooks-ts'
 
 const AboutMe = () => {
-    const isMobile = useMediaQuery("(max-width: 768px)");
 
-    const aboutMeString = "I am a full-stack web developer studying Computer Science at the University of Maryland. I have experience with React, NextJS, Java, Python, and many other technologies. Currently I am looking for internships and other opportunities"
-
-    const funFactString = "I love hiking and being in nature, I often times code in the woods behind my dorm as I find it more relaxing than a library or my room. I also really enjoy playing video games and have a strong interest in modding them"
+    const aboutMeString = "I am a full-stack web developer with a passion for making websites and using AI. I have experience with many different technologies in front and back end development. Currently, I am looking for internships and would love to connect so please reach out!"
 
 
   return (
@@ -19,15 +16,6 @@ const AboutMe = () => {
                 {aboutMeString}
             </h3>
         </div>
-        {!isMobile && 
-              <div className="p-5 rounded-lg mx-[10%] md:mx-[20%] text-muted font-medium text-lg md:text-[35px] md:leading-10 lg:text-[25px] xl:text-[35px] text-center">
-                <h3 className='underline underline-offset-4 pb-2 '>
-                    Some fun facts about me: 
-                </h3>
-                <h3>   
-                    {funFactString} 
-                </h3>
-            </div>}
     </div>
   )
 }
