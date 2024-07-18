408250ed1398922db8637493338ab6cc4f62e7ff
Will <willgraham367@gmail.com>
2024-07-18 02:35:36 -0400

    style: border has gaps on sides in mobile view

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,7 +14,7 @@ const Skill = (name: string, src: string, whiteBg?: boolean) => {
 const Skills = () => {
   return (
     <div className='flex items-center w-full h-full justify-center flex-col'>
-        <h1 className="text-muted font-medium text-[50px] border-b-2 px-4 text-center">
+        <h1 className="text-muted font-medium text-[50px] border-b-2 px-4 text-center max-w-[90%]">
             My Tech Stack
         </h1>
         <div className='flex justify-center flex-wrap max-w-6xl gap-10 py-10 pb-20'>
